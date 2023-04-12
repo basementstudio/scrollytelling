c2c96cb1a3bd4bec9978a0754cba68f40510d9b7
Nazareno Oviedo <nazareno@basement.studio>
2023-04-12 19:01:59 -0300

    feat: improves

--- website/src/app/sections/hero/mac-model.tsx
+++ website/src/app/sections/hero/mac-model.tsx
@@ -1,8 +1,9 @@
-import * as THREE from "three";
-import React from "react";
-import { Float, useGLTF } from "@react-three/drei";
-import { GLTF } from "three-stdlib";
-import { useThree } from "@react-three/fiber";
+import * as THREE from 'three';
+import { Float, useGLTF } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useAnimationHeroStore } from '.';
+import { useRef } from 'react';
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -15,33 +16,29 @@ type GLTFResult = GLTF & {
   };
 };
 
-useGLTF.preload("/models/Mac128k-light.glb");
+useGLTF.preload('/models/Mac128k-light.glb');
 
-export function MacModel(props: JSX.IntrinsicElements["group"]) {
-  const { nodes, materials } = useGLTF(
-    "/models/Mac128k-light.glb"
-  ) as GLTFResult;
+export const MacModel = () => {
+  const { nodes, materials } = useGLTF('/models/Mac128k-light.glb') as GLTFResult;
+  const innerRef = useRef<THREE.Group>(null!);
 
-  const width = useThree((state) => state.viewport.width); 
+  const width = useThree((state) => state.viewport.width);
+
+  const progress = useAnimationHeroStore((state) => state.progress);
+
+  useFrame(() => {
+    if (!innerRef.current) return;
+    innerRef.current.rotation.y = Math.PI * 2 * progress;
+  });
 
   return (
     <Float>
-      <group {...props} dispose={null} scale={width * 0.6}>
+      <group dispose={null} scale={width * 0.6} ref={innerRef}>
         <group position={[0, 0, 0]} rotation={[0.45, -0.51, -0.03]}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube009.geometry}
-            material={materials.m_Mac128k}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube009_1.geometry}
-            material={materials.m_Outline}
-          />
+          <mesh castShadow receiveShadow geometry={nodes.Cube009.geometry} material={materials.m_Mac128k} />
+          <mesh castShadow receiveShadow geometry={nodes.Cube009_1.geometry} material={materials.m_Outline} />
         </group>
       </group>
     </Float>
   );
-}
+};
