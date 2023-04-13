c161ca06435887c2aaaacff29919e28ec92160d5
Fede Álvarez <fedealvarezcampos@gmail.com>
2023-04-13 19:05:59 +0200

    Work on Footer

--- website/src/app/sections/footer/index.tsx
+++ website/src/app/sections/footer/index.tsx
@@ -1,66 +1,90 @@
 import Image from 'next/image';
 import bsmtTeamImg from '../../../../public/footer/basement-team-footer.jpg';
+import * as Scrollytelling from '@bsmnt/scrollytelling';
 import QRImg from '../../../../public/footer/QR.svg';
-
+import { toVw } from '../../../lib/utils';
 import s from './footer.module.scss';
 import Link from '../../components/link';
 import { DottedDiv } from '../../components/dotted-container';
+import basementTeamSVG from '../../../../public/footer/basement-team.svg';
 
 export const Footer = () => {
 	return (
-		<footer className={s.footer}>
-			<div className={s['pre-footer']}>
-				<div>
-					<p>Now we are talking! Say hello to our OSS brand new scrollytelling library</p>
+		<Scrollytelling.Root end="bottom bottom">
+			<footer className={s.footer}>
+				<div className={s['pre-footer']}>
+					<div className={s['left-content']}>
+						<p>Now we are talking! Say hello to our OSS brand new scrollytelling library</p>
 
-					<div className={s.terminal}>
-						<div className={s['upper-bar']}>
-							<span className={s.dots}>
-								{[1, 2, 3].map((_, idx) => (
-									<span key={idx} className={s.circle} />
-								))}
-							</span>
-							<span>Terminal</span>
+						<div className={s.terminal}>
+							<div className={s['upper-bar']}>
+								<span className={s.dots}>
+									{[1, 2, 3].map((_, idx) => (
+										<span key={idx} className={s.circle} />
+									))}
+								</span>
+								<span>Terminal</span>
+							</div>
+							<DottedDiv className={s.content}>
+								<p>yarn add @bsmnt/scrollytelling</p>
+							</DottedDiv>
 						</div>
-						<DottedDiv className={s.content}>
-							<p>yarn add @bsmnt/scrollytelling</p>
-						</DottedDiv>
+					</div>
+
+					<Scrollytelling.Animation
+						tween={{
+							start: 10,
+							end: 100,
+							fromTo: [
+								{
+									yPercent: 0,
+									height: toVw(436, 1920, 180),
+									width: toVw(436, 1920, 180),
+								},
+								{
+									yPercent: 113.8,
+									xPercent: 36,
+									height: toVw(620, 1920, 220),
+									width: toVw(620, 1920, 220),
+								},
+							],
+						}}
+					>
+						<Image className={s.QR} src={QRImg} alt="QR" />
+					</Scrollytelling.Animation>
+				</div>
+
+				<Image className={s['team-img']} src={bsmtTeamImg} alt="Basement Team" />
+				<Image className={s['footer-heading-text']} src={basementTeamSVG} alt="basement team" />
+				<div className={s.links}>
+					<div>
+						<span>social media</span>
+						<ul>
+							{socials.map((social, idx) => (
+								<>
+									{idx !== 0 && <span>&nbsp;—&nbsp;</span>}
+									<li key={social.name}>
+										<Link href={social.url}>{social.name}</Link>
+									</li>
+								</>
+							))}
+						</ul>
+					</div>
+					<div>
+						<span>get in touch</span>
+						<Link href="mailto:sayhi@basement.studio">sayhi@basement.studio</Link>
+					</div>
+					<div
+						style={{
+							alignItems: 'end',
+						}}
+					>
+						<span>@basement.studio llc {new Date().getFullYear()}</span>
+						<span>all rights reserved</span>
 					</div>
 				</div>
-
-				<Image className={s.QR} src={QRImg} alt="QR" />
-			</div>
-
-			<Image className={s['team-img']} src={bsmtTeamImg} alt="Basement Team" />
-			<h2>BSMNT TEAM</h2>
-			<div className={s.links}>
-				<div>
-					<span>social media</span>
-					<ul>
-						{socials.map((social, idx) => (
-							<>
-								{idx !== 0 && <span>&nbsp;—&nbsp;</span>}
-								<li key={social.name}>
-									<Link href={social.url}>{social.name}</Link>
-								</li>
-							</>
-						))}
-					</ul>
-				</div>
-				<div>
-					<span>get in touch</span>
-					<Link href="mailto:sayhi@basement.studio">sayhi@basement.studio</Link>
-				</div>
-				<div
-					style={{
-						alignItems: 'end',
-					}}
-				>
-					<span>@basement.studio llc {new Date().getFullYear()}</span>
-					<span>all rights reserved</span>
-				</div>
-			</div>
-		</footer>
+			</footer>
+		</Scrollytelling.Root>
 	);
 };
 
