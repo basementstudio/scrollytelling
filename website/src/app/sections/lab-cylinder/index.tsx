5c31b033bed432254cf5d2c7a11eabb62b2e4e52
Julian Benegas <julianbenegas99@gmail.com>
2023-04-14 16:10:18 -0300

    some reorders

--- website/src/app/sections/lab-cylinder/index.tsx
+++ website/src/app/sections/lab-cylinder/index.tsx
@@ -1,3 +1,15 @@
-export const LabCylinderSection = () => {
-  return null;
+import { Experiment } from "../../../lib/types";
+import { Cyllinder } from "../../components/cyllinder";
+
+export const LabCylinder = async () => {
+  const experiments = await fetch(
+    "https://lab.basement.studio/experiments.json",
+    { next: { revalidate: 1 } }
+  ).then((res) => res.json());
+
+  const filteredExperiments = experiments.filter(
+    (experiment: any) => experiment.og !== null
+  ) as Experiment[];
+
+  return <Cyllinder experiments={filteredExperiments} />;
 };
