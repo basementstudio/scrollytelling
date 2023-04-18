7f3429847277e623a3209ae5a7d30fcadbc85680
Nazareno Oviedo <nazareno@basement.studio>
2023-04-18 12:06:38 -0300

    feat: add metadata

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./css/global.scss";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { JetBrains_Mono } from "next/font/google";
+import { siteOrigin } from "~/lib/constants";
 
 const jetBrainsMono = JetBrains_Mono({
   weight: "400",
@@ -23,7 +24,34 @@ const basementGrotesque = localFont({
 
 export const metadata: Metadata = {
   title: "BSMNT @ React Miami Conf",
-  description: "to-do",
+  description: "BSMNT @ React Miami Conf",
+  viewport: {
+    height: "device-height",
+    initialScale: 1,
+    width: "device-width",
+  },
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+  },
+  manifest: "/manifest.webmanifest",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@basementstudio",
+    description: "BSMNT @ React Miami Conf",
+    images: [{ width: 1200, height: 630, url: `${siteOrigin}/og.jpeg` }],
+    site: "@basementstudio",
+    title: "BSMNT @ React Miami Conf",
+  },
+  openGraph: {
+    description: "BSMNT @ React Miami Conf",
+    images: [{ width: 1200, height: 630, url: `${siteOrigin}/og.jpeg` }],
+    locale: "en-US",
+    siteName: "BSMNT @ React Miami Conf",
+    title: "BSMNT @ React Miami Conf",
+    type: "website",
+    url: siteOrigin,
+  },
 };
 
 export default function RootLayout({
