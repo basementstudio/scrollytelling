54f314a28fe5b517b9ef70dafe933c527fedf24b
Julian Benegas <julianbenegas99@gmail.com>
2023-07-24 19:37:17 -0300

    change gsap import for cjs

--- scrollytelling/tsup.config.ts
+++ scrollytelling/tsup.config.ts
@@ -7,9 +7,20 @@ const prependUseClientPlugin = {
       result.outputFiles
         ?.filter((file: any) => !file.path.endsWith(".map"))
         .forEach(async (file: any) => {
+          // add 'use client' for RSC
           Object.defineProperty(file, "text", {
             value: `"use client";\n${file.text}`,
           });
+          if (file.path.endsWith(".js")) {
+            // change gsap import to point to the cjs one https://greensock.com/forums/topic/26104-nuxt-gsapdraggable-cannot-use-import-statement-outside-a-module/
+            // and https://github.com/basementstudio/scrollytelling/issues/33
+            Object.defineProperty(file, "text", {
+              value: file.text.replace(
+                "gsap/ScrollTrigger",
+                "gsap/dist/ScrollTrigger"
+              ),
+            });
+          }
         });
     });
   },
