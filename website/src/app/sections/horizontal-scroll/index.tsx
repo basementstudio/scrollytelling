baeb0ff088a4b49c97558462258214e9e556fa33
Nazareno Oviedo <nazareno@basement.studio>
2023-10-30 14:45:20 -0300

    feat: clean code

--- website/src/app/sections/horizontal-scroll/index.tsx
+++ website/src/app/sections/horizontal-scroll/index.tsx
@@ -7,23 +7,57 @@ import { useIsoLayoutEffect } from "~/app/hooks/use-iso-layout-effect";
 
 import s from "./horizontal-scroll.module.scss";
 
+// Define the sections for the animation
+const sectionData = [
+  { label: "SECTION 1" },
+  { label: "SECTION 2" },
+  { label: "SECTION 3" },
+  { label: "SECTION 4" },
+  { label: "SECTION 5" },
+];
+
 export const HorizontalScroll = () => {
+  // Ref for the main container
   const containerRef = useRef<HTMLDivElement>(null);
+
+  // State to store the inner sections
   const [sections, setSections] = useState<HTMLDivElement[]>([]);
 
+  // Effect to initialize the sections
   useIsoLayoutEffect(() => {
     if (!containerRef.current) return;
 
-    const q = gsap.utils.selector(containerRef);
-    const inner_sections = q(`.${s["panel"]}`) as HTMLDivElement[];
+    // Use a selector to find the inner sections
+    const scopedQuerySelector = gsap.utils.selector(containerRef);
+    const inner_sections = scopedQuerySelector(
+      `.${s["panel"]}`
+    ) as HTMLDivElement[];
 
     setSections(inner_sections);
   }, []);
 
-  const tween = useMemo(() => {
+  // Calculate animation configuration for horizontal scroll
+  const horizontalScrollTween = useMemo(() => {
     return { xPercent: -100 * (sections.length - 1), ease: "none" };
   }, [sections.length]);
 
+  // Create animated section elements
+  const sectionElements = sectionData.map((section, index) => (
+    <Scrollytelling.Animation
+      key={index}
+      tween={{
+        start: 0,
+        end: 100,
+        to: horizontalScrollTween,
+      }}
+    >
+      <div className={s["panel"]}>
+        <h2>{section.label}</h2>
+      </div>
+    </Scrollytelling.Animation>
+  ));
+
+  // Main component with horizontal scroll effect
   return (
     <Scrollytelling.Root
       defaults={{ ease: "linear" }}
@@ -38,61 +72,7 @@ export const HorizontalScroll = () => {
         childClassName={s["pin-style"]}
         ref={containerRef}
       >
-        <Scrollytelling.Animation
-          tween={{
-            start: 0,
-            end: 100,
-            to: tween,
-          }}
-        >
-          <div className={s["panel"]}>
-            <h2>SECTION 1</h2>
-          </div>
-        </Scrollytelling.Animation>
-        <Scrollytelling.Animation
-          tween={{
-            start: 0,
-            end: 100,
-            to: tween,
-          }}
-        >
-          <div className={s["panel"]}>
-            <h2>SECTION 2</h2>
-          </div>
-        </Scrollytelling.Animation>
-        <Scrollytelling.Animation
-          tween={{
-            start: 0,
-            end: 100,
-            to: tween,
-          }}
-        >
-          <div className={s["panel"]}>
-            <h2>SECTION 3</h2>
-          </div>
-        </Scrollytelling.Animation>
-        <Scrollytelling.Animation
-          tween={{
-            start: 0,
-            end: 100,
-            to: tween,
-          }}
-        >
-          <div className={s["panel"]}>
-            <h2>SECTION 4</h2>
-          </div>
-        </Scrollytelling.Animation>
-        <Scrollytelling.Animation
-          tween={{
-            start: 0,
-            end: 100,
-            to: tween,
-          }}
-        >
-          <div className={s["panel"]}>
-            <h2>SECTION 5</h2>
-          </div>
-        </Scrollytelling.Animation>
+        {sectionElements}
       </Scrollytelling.Pin>
     </Scrollytelling.Root>
   );
