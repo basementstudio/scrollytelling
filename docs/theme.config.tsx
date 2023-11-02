bdd1e0447d7474a684c252c56ffdc1cde8e919c7
Fede Álvarez <fedealvarezcampos@gmail.com>
2023-11-02 10:03:11 +0100

    Meta and thing

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>basement scrollytelling</span>,
@@ -15,6 +17,14 @@ const config: DocsThemeConfig = {
   },
   primaryHue: 21,
   primarySaturation: 94,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – BSMNT Scrollytelling',
+      }
+    }
+  }
 }
 
 export default config
